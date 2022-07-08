c6b0061a14e3b2bfbda653bf688436296bb8e368
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-08 22:01:17 +1000

    added babel and css loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,16 +3,17 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
 	return {
 		mode: "development",
+		// Entry point for files
 		entry: {
 			main: "./src/js/index.js",
 			install: "./src/js/install.js",
 		},
+		// Output for our bundles
 		output: {
 			filename: "[name].bundle.js",
 			path: path.resolve(__dirname, "dist"),
@@ -52,7 +53,25 @@ module.exports = () => {
 		],
 
 		module: {
-			rules: [],
+			// CSS loaders
+			rules: [
+				{
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+				},
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					// We use babel-loader in order to use ES6.
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"],
+						},
+					},
+				},
+			],
 		},
 	};
 };
