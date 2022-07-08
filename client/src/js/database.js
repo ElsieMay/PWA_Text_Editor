0cb3fcf92d28d0dff7b7b7d8c6f473080e62efda
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-08 21:59:39 +1000

    added workbox plugins

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
 		},
 	});
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 // Export a function we will use to POST to the database.
 export const putDb = async (content) => {
 	console.log("Post to the database");
@@ -35,7 +34,6 @@ export const putDb = async (content) => {
 };
 // export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
 // Export a function we will use to GET to the database.
 export const getDb = async () => {
 	console.log("GET from the database");
